6379a75ca12f1b751ac371bd193f76e460477de1
Elias Stenhede <elias.stenhede@gmail.com>
2025-12-10 10:16:19 +0100

    refactor: remove unusued code

--- utils.py
+++ utils.py
@@ -11,21 +11,6 @@ def silverman_bandwidth(x):
     return 0.9 * sigma * n ** (-1 / 5)
 
 
-def kernel_mean_std(x, y, x_grid):
-    """Gaussian Nadaraya–Watson."""
-    x = np.asarray(x, float)
-    y = np.asarray(y, float)
-    h = silverman_bandwidth(x)
-    U = (x[:, None] - x_grid[None, :]) / h
-    w = np.exp(-0.5 * U**2)
-
-    wsum = w.sum(axis=0)
-    mu = (w * y[:, None]).sum(axis=0) / wsum
-    m2 = (w * (y[:, None] ** 2)).sum(axis=0) / wsum
-    std = np.sqrt(np.maximum(m2 - mu**2, 0.0))
-    return mu, std
-
-
 def _weighted_quantile(v, w, qs):
     idx = np.argsort(v)
     v_sorted = v[idx]
@@ -48,23 +33,6 @@ def rank_uniform(x):
 def inv_ecdf(u, x):
     u = np.asarray(u, float)
     return np.quantile(x, u, method="linear")
-
-
-# def smooth_prob_curve_rankx(x, p, n_grid=200):
-#     x = np.asarray(x, float)
-#     eps = 1e-5
-#     p = np.clip(np.asarray(p, float), eps, 1 - eps)
-#     u = rank_uniform(x)
-#     u_grid = np.linspace(0.001, 0.999, n_grid)
-
-#     z = np.log(p / (1 - p))  # logit
-#     muz, sdz = kernel_mean_std(u, z, x_grid=u_grid)
-#     sigmoid = lambda t: 1 / (1 + np.exp(-t))
-#     mean_p = sigmoid(muz)
-#     lo = sigmoid(muz - sdz)
-#     hi = sigmoid(muz + sdz)
-#     xg_real = inv_ecdf(u_grid, x)
-#     return xg_real, mean_p, lo, hi
 
 
 def kernel_quantile_band_rankx(x, y, qs=(0.25, 0.75), n_grid=200):
